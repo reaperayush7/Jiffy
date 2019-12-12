60ea368e14367172a6b7489290b41d4a20ae18eb
reaperayush7 <reaper.ayush7@gmail.com>
2019-12-12 08:09:02 +0545

    login design and static validation

--- app/src/main/java/com/example/jiffy/LoginActivity.java
+++ app/src/main/java/com/example/jiffy/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.jiffy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,37 +9,51 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
+
 public class LoginActivity extends AppCompatActivity {
+
+    ConstraintLayout constraintLayout;
+
+    TextInputLayout textInputLayoutEmail, textInputLayoutPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        final EditText etEmail, etPassword;
-        Button btnLogin;
-
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
 
-        etEmail = findViewById( R.id.etEmail );
-        etPassword = findViewById( R.id.etPassword );
         btnLogin = findViewById( R.id.btnLogin );
+        constraintLayout = findViewById( R.id.constraintContainer );
+
+        Calendar calender = Calendar.getInstance();
+
+        int timeOfDay = calender.get(Calendar.HOUR_OF_DAY);
+
+        if (timeOfDay >= 6 && timeOfDay <= 18)
+        {
+            constraintLayout.setBackground( getDrawable( R.drawable.good_morning_img ) );
+        }
+        else
+        {
+            constraintLayout.setBackground( getDrawable( R.drawable.good_night_img ) );
+        }
+
 
         btnLogin.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (etEmail.getText().toString().equals( "admin@admin.com" ) && etPassword.getText().toString().equals( "admin" ))
-                {
-                 startActivity( new Intent( LoginActivity.this, DashboardActivity.class ));
-                 finish();
-                }
-                else {
-                    etEmail.setText( "" );
-                    etPassword.setText( "" );
-                    etEmail.setError( "Email or Password does not match our data" );
-                }
-
+                startActivity( new Intent( getApplicationContext(), DashboardActivity.class ) );
             }
         } );
+
+
+
+
+
 
     }
 }
